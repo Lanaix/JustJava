96bb56ed353e66e77ecb5b1f14f256f1bab48560
666200 <666200@student.dmschools.org>
2017-10-20 09:42:22 -0500

    ok

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -21,7 +21,7 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity=0;
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,21 +49,33 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity*5);
+        int price = calculatePrice();
+        String priceMessage = "Total: $" + price;
+        priceMessage = priceMessage + "\nThank you!";
+        displayMessage(priceMessage);
 
+    }
+    /**
+     * Calculates the price of the order.
+     *
+     *  @return total price
+     */
+    private int calculatePrice(){
+        int price = quantity * 5;
+        return price;
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        TextView quantityTextView = (TextView) findViewById(R.id.price_text_view);
         quantityTextView.setText("" + number);
     }
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
+    private void displayMessage(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
